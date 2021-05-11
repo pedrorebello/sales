16026b3ef372d406dcb99279ef3f30da58f7921b
pedrorebello <65690682+pedrorebello@users.noreply.github.com>
2021-05-11 14:34:57 -0700

    add search to be used by charts

--- backend/src/main/java/com/example/sales/repositories/SaleRepository.java
+++ backend/src/main/java/com/example/sales/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.example.sales.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.sales.dto.SaleSuccessDTO;
+import com.example.sales.dto.SaleSumDTO;
 import com.example.sales.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
+	@Query("SELECT new com.example.sales.dto.SaleSumDTO(obj.seller, SUM(obj.amount))"
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+
+	@Query("SELECT new com.example.sales.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals))"
+			+ " FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
--- backend/src/main/java/com/example/sales/services/SaleService.java
+++ backend/src/main/java/com/example/sales/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.example.sales.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.sales.dto.SaleDTO;
+import com.example.sales.dto.SaleSuccessDTO;
+import com.example.sales.dto.SaleSumDTO;
 import com.example.sales.entities.Sale;
 import com.example.sales.repositories.SaleRepository;
 import com.example.sales.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll(); //avoid repeated db calls
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
 	}
 }
