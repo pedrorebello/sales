a91be2bc60d35e3a76a97a060d4d5d09707e9d9a
pedrorebello <65690682+pedrorebello@users.noreply.github.com>
2021-05-11 13:09:09 -0700

    Add seller repository/service/dto/controller

---
+++ backend/src/main/java/com/example/sales/services/SellerService.java
@@ -0,0 +1,23 @@
+package com.example.sales.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.sales.dto.SellerDTO;
+import com.example.sales.entities.Seller;
+import com.example.sales.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
