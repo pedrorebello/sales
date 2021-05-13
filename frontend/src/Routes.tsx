cece88e652b0334371e8668e56d2ac8ae96eb7ed
pedrorebello <65690682+pedrorebello@users.noreply.github.com>
2021-05-12 17:06:26 -0700

    reverting to commit a8e6540

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,7 +1,7 @@
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
-import DashBoard from './pages/DashBoard';
-import Home from './pages/Home';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
+import DashBoard from 'pages/DashBoard';
+import Home from 'pages/Home';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const Routes = () => (
